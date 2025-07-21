bac1a20c7b437e91d38b8c489656a36685e39da4
Pyrus-Ovn <asger.tougaard.mikkelsen@gmail.com>
2025-07-21 12:18:18 +0200

    discman 1.0 working

---
+++ Scenes/Ui/ui_discman.gd
@@ -0,0 +1,31 @@
+extends Control
+@onready var discer: Control = $Discer
+@onready var song_label: Label = $Discer/Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Connect to the autoload music player
+	Globaldiscman.connect("song_changed", _on_song_changed)
+# Update immediately if song is playing
+	if Globaldiscman.current_song_index != -1:
+		_on_song_changed(Globaldiscman.get_current_song_name(), Globaldiscman.current_song_index)
+
+func _on_song_changed(song_name: String, song_index: int):
+	song_label.text = song_name
+
+
+
+func _on_hide_button_toggled(toggled_on: bool) -> void:
+	discer.visible = toggled_on
+
+
+func _on_play_button_pressed() -> void:
+	Globaldiscman.play() # Replace with function body.
+
+
+func _on_forward_button_pressed() -> void:
+	Globaldiscman.play_next()
+
+
+func _on_back_button_pressed() -> void:
+	Globaldiscman.play_previous()
