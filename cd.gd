da2a7a4f7d0edcb5a1ccfebf3afb1bb6325f5a3c
skoleskakwack <kristianb.wilson@gmail.com>
2025-07-21 21:06:21 +0200

    sfx cd spin og pickup

--- cd.gd
+++ cd.gd
@@ -1,11 +1,20 @@
 extends Node3D
+@onready var scaler: Node3D = $Scaler
+
+@onready var cd_sfx: AudioStreamPlayer3D = $cdSFX
 
 
+@onready var pickup_sfx: AudioStreamPlayer3D = $PickupSFX
 
 
 
 func _on_area_3d_body_entered(_player: Node3D) -> void:
 	#add data to discman
 	#play crazy effects
+	cd_sfx.stop()
+	pickup_sfx.play()
+	scaler.visible = false
+	await  pickup_sfx.finished
+	
 	queue_free()
 	
