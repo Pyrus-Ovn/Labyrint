4f02ffd8a10e04e6571f208cd74c11ad42f335e6
Madsbglock <madsbglock@gmail.com>
2025-07-23 11:03:43 +0200

    cd'er
    cd'er

--- Scenes/Ui/ui_discman.gd
+++ Scenes/Ui/ui_discman.gd
@@ -1,8 +1,7 @@
 extends Control
 @onready var discer: Control = $Discer
-@onready var song_label: Label = $Discer/Label
 @onready var clicksfx: AudioStreamPlayer = $clicksfx
-
+@onready var song_label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
